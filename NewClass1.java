985716b6fca01343e77f17ff54456d20e61cc171
larrytunes <thorllaniyi@gmail.com>
2016-12-06 23:31:58 +0100

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,9 @@
+package csc309Assignment;
+public class MyProfile {
+    
+    public static void main(String args[])
+    {
+        System.out.println("Olaniyi Gideon Olamide");
+        System.out.println("CSC/2014/0050");
+    }
+}
